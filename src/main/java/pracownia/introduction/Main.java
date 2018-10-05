7ab20bbc9270b920ec9623f2f79a254030a93e22
przand1 <przand1@st.amu.edu.pl>
2018-10-05 14:45:52 +0200

    Commit 1

--- src/main/java/pracownia/introduction/Main.java
+++ src/main/java/pracownia/introduction/Main.java
@@ -1,8 +1,12 @@
 package pracownia.introduction;
 
+import org.apache.log4j.Logger;
+
 public class Main {
+private static Logger log= Logger.getLogger(Main.class);
 
     public static void main(String [ ] args) {
+        log.info("message");
         System.out.println("Hello World");
     }
 }
